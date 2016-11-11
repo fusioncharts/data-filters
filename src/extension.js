da7ea2aed6a369ea1ed3b4fa6ac4ec20d5723747
Pratyush <pratyush.creed@gmail.com>
2016-11-11 14:19:30 +0530

    lint update

--- src/extension.js
+++ src/extension.js
@@ -2,5 +2,20 @@ class FCDataFilterExt {
   constructor () {
     var multiChart = new MultiChart();
     this.datastore = multiChart.createDataStore();
+    this.dataProcessor = multiChart.createDataProcessor();
+    // setting demo data
+    this.datastore.setData({
+      dataSource: [{
+        product: 'tea',
+        sale: '45',
+        year: 2016
+      },
+      {
+        product: 'coffee',
+        sale: '45',
+        year: 2016
+      }]
+    });
+    // data set
   }
 }
