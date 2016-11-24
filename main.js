93be1fea1c0c07475401eb109398c6a12fd636b1
Pratyush <pratyush.creed@gmail.com>
2016-11-24 14:13:12 +0530

    added table

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 
 let multicharting = new MultiCharting(),
 	datastore = multicharting.createDataStore(),
+	table = multicharting.dataTable(datastore, 'table-container'),
 	filter;
 
-datastore.setData({dataSource: data});
-debugger;
+datastore.setData({dataSource: data.slice(0, 100)});
 filter = new FCDataFilterExt(datastore, {}, 'filter-container');
